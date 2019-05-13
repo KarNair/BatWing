e730c796b5e2b089c6761b83c82dde24c93017b2
KarNair <karthikkuttan@gmail.com>
2019-05-13 21:41:18 +0200

    fastqc update

--- BatCode/fastqc.sh
+++ BatCode/fastqc.sh
@@ -2,15 +2,15 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:05:00
+#SBATCH -t 00:10:00
 #SBATCH -J run_fastqc
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karthik.nair1194@gmail.com
 # Load modules
+module load samtools
 module load bioinfo-tools
 module load FastQC/0.11.5
-# Your commands
-for filename in  /home/karnair/GenomeAnalysis/BatWing/Data/raw_data/rna_seq/raw/*.fastq.gz
+for filename in /home/karnair/GenomeAnalysis/BatWing/Data/raw_data/rna_seq/raw/*.fastq.gz
     do
         fastqc $filename -o /home/karnair/GenomeAnalysis/BatWing/results;
 done
